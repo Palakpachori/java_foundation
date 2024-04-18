876803dd97e0921edca437158af05eb95df67073
Palakpachori <159933033+Palakpachori@users.noreply.github.com>
2024-04-18 23:02:52 +0530

    Add files via upload

---
+++ PrintOneToHundred.java
@@ -0,0 +1,12 @@
+public class PrintOneToHundred{
+    public static void main(String[] args){
+        int one = 'A'/'A'; //1
+        String s1 = "..........";
+         //for(int i=one; i<=(s1.length() * s1.length()); i++) 
+         // prints no. from 1 to 100 without using any no.
+         for(int i=one; i<='d'; i+=one) 
+         System.out.println(i);
+    }
+
+    
+}
